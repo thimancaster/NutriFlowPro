1238ff89ba66abb514ec895d536b64ada594e691
Thiago Ferreira <thimancaster@hotmail.com>
2025-09-29 16:32:00 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,31 @@
-import React from "react";
-import {Toaster} from "@/components/ui/sonner";
-import {TooltipProvider} from "@/components/ui/tooltip";
-import OptimizedProviders from "./components/OptimizedProviders";
-import AppRoutes from "./routes";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from '@/components/theme-provider';
+import { AuthProvider } from '@/contexts/auth/AuthContext';
+import { ClinicalWorkflowProvider } from '@/contexts/ClinicalWorkflowContext';
+import AppRoutes from '@/routes';
+import { Toaster } from '@/components/ui/sonner';
+import GlobalErrorBoundary from './components/error/GlobalErrorBoundary';
 
-const App = () => (
-	<OptimizedProviders>
-		<TooltipProvider>
-			<Toaster />
-			<AppRoutes />
-		</TooltipProvider>
-	</OptimizedProviders>
-);
+/**
+ * Componente raiz da aplicação.
+ * Responsável por configurar os provedores de contexto globais e o sistema de rotas.
+ */
+function App() {
+  return (
+    <GlobalErrorBoundary>
+      <Router>
+        <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+          <AuthProvider>
+            {/* O ClinicalWorkflowProvider gerencia a sessão clínica ativa */}
+            <ClinicalWorkflowProvider>
+              <AppRoutes />
+            </ClinicalWorkflowProvider>
+          </AuthProvider>
+          <Toaster />
+        </ThemeProvider>
+      </Router>
+    </GlobalErrorBoundary>
+  );
+}
 
 export default App;
