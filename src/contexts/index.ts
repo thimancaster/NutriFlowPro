fc39f72565207adad6d0d4802468e66b00ec472e
Thiago Ferreira <thimancaster@hotmail.com>
2025-09-30 14:11:15 -0300

    Update index.ts

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,11 +1,4 @@
-
-// Re-export components from the contexts
-export { AuthProvider, useAuth } from './auth/AuthContext';
-export { PatientProvider, usePatient } from './patient/PatientContext';
-export { MealPlanProvider } from './MealPlanContext';
-export { ConsultationProvider } from './ConsultationContext'; 
-export { ConsultationDataProvider, useConsultationData } from './ConsultationDataContext';
-// export { ThemeProvider } from '@/contexts/theme/ThemeProvider';
-
-// Re-export types for convenience
-export type { AuthContextType, AuthState } from './auth/types';
+// src/contexts/index.ts
+export * from './AuthContext';
+export * from './ClinicalWorkflowContext';
+export * from './UnifiedNutritionContext';
