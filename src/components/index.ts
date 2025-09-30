65f10a7ee178adab640fe944ee9b3d5def4fb6f0
Thiago Ferreira <thimancaster@hotmail.com>
2025-09-30 14:12:04 -0300

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,23 +1,10 @@
+// src/components/index.ts
 
-// Export all components for easy imports
-export { default as ConsultationHeader } from './ConsultationHeader';
-export { default as MealPlanActions } from './meal-plan/MealPlanActions';
-export { BirthDatePicker } from './ui/birth-date-picker';
-export { default as MealSummaryCard } from './MealPlan/MealSummaryCard';
-export { default as NutritionSummaryHeader } from './MealPlan/NutritionSummaryHeader';
-export { default as MealPlanActionButtons } from './MealPlan/MealPlanActionButtons';
-export { default as MealPlanAssemblyCard } from './MealPlan/MealPlanAssemblyCard';
-export { default as MealPlanLoader } from './MealPlan/MealPlanLoader';
-export { default as MealPlanHeader } from './MealPlan/MealPlanHeader';
-export { default as MealPlanMacroSummary } from './MealPlan/MealPlanMacroSummary';
-export { default as ConsultationForm } from './Consultation/ConsultationForm';
-export { default as ConsultationResults } from './Consultation/ConsultationResults';
-
-// Calculator components exports - Updated to use the modular calculator
-export { 
-  CalculatorTool, 
-  CalculatorInputs, 
-  MacroDistributionInputs, 
-  CalculatorResults,
-  CalculatorActions
-} from './calculator';
+export { default as AnthropometryForm } from './Anthropometry/AnthropometryForm';
+export { default as AnthropometryHistory } from './Anthropometry/AnthropometryHistory';
+export { default as Layout } from './Layout';
+export { default as Navbar } from './Navbar';
+export { default as PatientForm } from './PatientForm';
+export { default as PatientHistory } from './PatientHistory';
+export { default as Patients } from './Patients';
+export { default as ProtectedRoute } from './ProtectedRoute';
