7e9cc85f36cf3c578dc63e7e5f57bb1c44d71385
Thiago Ferreira <thimancaster@hotmail.com>
2025-10-06 11:17:42 -0300

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,17 @@
 import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
+import Navbar from './navbar';
 import { Toaster } from './ui/sonner';
 
 /**
  * Componente principal de Layout para a área logada da aplicação.
- * Inclui a barra de navegação e renderiza o conteúdo da rota ativa através do <Outlet /> ou children.
+ * Inclui a barra de navegação e renderiza o conteúdo da rota ativa através do <Outlet />.
  */
-export default function Layout({ children }: { children?: React.ReactNode }) {
+export default function Layout() {
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        {children || <Outlet />}
+        <Outlet />
       </main>
       <Toaster />
     </div>
